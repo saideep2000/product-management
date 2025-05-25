812b07419bea406f159b1dd1586b59a5bd189a80
saideepsamineni <saminenisaideepchowdary@gmail.com>
2025-05-25 17:07:03 -0400

    done

--- main.js
+++ main.js
@@ -3,13 +3,12 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Socks',
+            selectedVariant: 0,
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
     },
@@ -17,8 +16,18 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
         }
+        
+    },
+    computed: {
+        image(){
+            this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            this.variants[this.selectedVariant].quantity
+        } 
     }
+
 })
